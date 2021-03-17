a66b71e070742a041aaae74e09d1c81b07f145d4
sudarshan gupta <sudarshan395@gmail.com>
2021-03-17 05:32:44 +0000

    DIsplay validation messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,25 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def Index
+  def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+   #render plain: params[:article]
+   @article = Article.new(params.require(:article).permit(:title, :description))
+   if @article.save
+    flash[:notice] = "Article was created successfully"
+   redirect_to @article
+   else
+    render 'new'
+   end
+   
+
   end
 
 end
