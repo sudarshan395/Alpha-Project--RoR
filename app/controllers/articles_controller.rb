991ee5c86065f3952844d7d9ebc9f5be795cb86d
sudarshan gupta <sudarshan395@gmail.com>
2021-03-18 11:44:19 +0000

    Partials Added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show 
-    @article = Article.find(params[:id])
+    
   end
 
   def index
@@ -12,12 +15,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-   @article = Article.find(params[:id])
+   
   end
 
   def create
    #render plain: params[:article]
-   @article = Article.new(params.require(:article).permit(:title, :description))
+   @article = Article.new(article_params)
    if @article.save
     flash[:notice] = "Article was created successfully"
     redirect_to @article
@@ -27,8 +30,8 @@ class ArticlesController < ApplicationController
   end
 
    def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    
+    if @article.update(article_params)
      flash[:notice] = "Article was updated successfully"
      redirect_to @article
     else
@@ -37,9 +40,19 @@ class ArticlesController < ApplicationController
    end
    
    def destroy
-    @article = Article.find(params[:id])
+    
     @article.destroy
     redirect_to articles_path
+   end
+
+   private 
+
+   def set_article
+    @article = Article.find(params[:id])
+   end
+
+    def article_params
+    params.require(:article).permit(:title, :description)
    end
 
 end
