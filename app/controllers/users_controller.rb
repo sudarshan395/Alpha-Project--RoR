3b10f8747a4d527a459ef0de3769395767b27262
sudarshan gupta <sudarshan395@gmail.com>
2021-03-27 17:28:52 +0000

    Admin controls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-before_action :set_user, only: [:show, :edit, :update]
+before_action :set_user, only: [:show, :edit, :update, :destroy]
 before_action :require_user, only: [:edit, :update]
-before_action :require_same_user, only: [:edit, :update]
+before_action :require_same_user, only: [:edit, :update, :destroy]
 
 def show
   @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -39,6 +39,13 @@ def create
   end
 end
 
+def destroy
+  @user.destroy
+  session[:user_id] = nil if @user == current_user
+  flash[:notice] = "Account successfully deleted"
+  redirect_to articles_path
+end
+
 private
 def user_params
   params.require(:user).permit(:username, :email, :password)
@@ -49,8 +56,8 @@ def set_user
 end
 
 def require_same_user
-    if current_user != @user
-      flash[:alert] = "You can only edit your own account"
+    if current_user != @user && !current.user.admin?
+      flash[:alert] = "You can only edit or delete your own account"
       redirect_to @user
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_062418) do
+ActiveRecord::Schema.define(version: 2021_03_27_132528) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_03_25_062418) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
