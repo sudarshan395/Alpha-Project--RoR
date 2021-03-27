741a757552f0daa8e8b48e06ba7af5a9b23f9177
sudarshan gupta <sudarshan395@gmail.com>
2021-03-27 08:57:36 +0000

    Create and destroy user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+  #post 'users', to: 'users#create'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
