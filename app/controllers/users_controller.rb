b0dcfccc3c03cf04cefe9b170c9fba1064441e76
sudarshan gupta <sudarshan395@gmail.com>
2021-03-27 12:44:19 +0000

    Restrict actions for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,58 @@
 class UsersController < ApplicationController
 
-  def show
-  @user = User.find(params[:id])
+before_action :set_user, only: [:show, :edit, :update]
+before_action :require_user, only: [:edit, :update]
+before_action :require_same_user, only: [:edit, :update]
+
+def show
   @articles = @user.articles.paginate(page: params[:page], per_page: 5)
-  end
+end
 
-  def index 
-    @users = User.paginate(page: params[:page], per_page: 5)
-  end
-  
- def new
+def index 
+  @users = User.paginate(page: params[:page], per_page: 5)
+end
+
+def new
   @user = User.new
- end
+end
 
- def edit
-  @user = User.find(params[:id])
- end
+def edit
+end
 
- def update
-  @user = User.find(params[:id])
+def update
   if @user.update(user_params)
   flash[:notice] = "Your account info is successfully updated"
   redirect_to @user
-  else
-    render 'edit'
-  end
- end
+else
+  render 'edit'
+end
+end
 
- def create
+def create
   @user = User.new(user_params)
   if @user.save
+    session[:user_id] = @user.id
     flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
     redirect_to articles_path
   else
-   render 'new'
+    render 'new'
   end
- end
+end
 
- private
- def user_params
-    params.require(:user).permit(:username, :email, :password)
- end
+private
+def user_params
+  params.require(:user).permit(:username, :email, :password)
+end
 
+def set_user
+  @user = User.find(params[:id])
+end
+
+def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account"
+      redirect_to @user
+    end
+end
 end
 
